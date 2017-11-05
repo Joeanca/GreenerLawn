7cb18b4fd68a53d768434a27c55b8ae8799bb8e4
keposet <jsn.hutson@gmail.com>
2017-11-05 13:07:16 -0700

    commented constructors
    added gets/sets for fields
    added addToSchedule method for incremental schedule modificaiton

--- app/src/main/java/com/greenerlawn/greenerlawn/Zones.java
+++ app/src/main/java/com/greenerlawn/greenerlawn/Zones.java
@@ -13,15 +13,53 @@ public class Zones {
     private boolean zOnOff;
     private ArrayList<zSchedule> zoneSchedule = new ArrayList<zSchedule>();
 
+    //default constructor for empty zone
     public Zones(String zGUID){
         this.zGUID = zGUID;
     }
+
+    //constructor allows for full zone config
     public Zones(String zGUID, String zName, boolean zOnOff) {
         this.zGUID = zGUID;
         this.zName = zName;
         this.zOnOff = zOnOff;
+    }
 
-        }
+    public String getzGUID() {
+        return zGUID;
+    }
+
+    public void setzGUID(String zGUID) {
+        this.zGUID = zGUID;
+    }
+
+    public String getzName() {
+        return zName;
+    }
+
+    public void setzName(String zName) {
+        this.zName = zName;
+    }
+
+    public boolean iszOnOff() {
+        return zOnOff;
+    }
+
+    public void setzOnOff(boolean zOnOff) {
+        this.zOnOff = zOnOff;
+    }
+
+    public ArrayList<zSchedule> getZoneSchedule() {
+        return zoneSchedule;
+    }
+
+    public void setZoneSchedule(ArrayList<zSchedule> zoneSchedule) {
+        this.zoneSchedule = zoneSchedule;
+    }
+
+    public void addToSchedule(int aDay, Long aStartTime, Long aDuration){
+        zoneSchedule.add(new zSchedule(aDay,aStartTime,aDuration));
+    }
 
     private class zSchedule{
         private int day;
