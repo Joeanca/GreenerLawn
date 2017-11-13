64c0fc63a4b36a083622874feb8b9b126a7454ec
keposet <jsn.hutson@gmail.com>
2017-11-13 14:01:45 -0700

    extract method for pulling zoneData from constructor

--- app/src/main/java/com/greenerlawn/greenerlawn/DataManager.java
+++ app/src/main/java/com/greenerlawn/greenerlawn/DataManager.java
@@ -11,6 +11,11 @@ public class DataManager {
     private ArrayList<Zone> zoneArrayList = new ArrayList<>();
 
     public DataManager() {
+        pullZoneData();
+    }
+
+    //TODO set method to pull from firebse
+    private void pullZoneData() {
         String[] guidArray = {"aa","ab","ac","ad","ae","af","ag","ah","ai"};
         String[] nameArray = {"name1","name2","name3","name4","name5","name6","name7","name8"};
         boolean status = false;
