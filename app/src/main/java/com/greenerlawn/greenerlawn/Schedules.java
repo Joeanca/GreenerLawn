4f81b74a0a4249aa1f867590bfe2e2bc80362ede
keposet <jsn.hutson@gmail.com>
2017-11-14 17:29:09 -0700

    removed schedule inner class from zone placed in schedules class

--- app/src/main/java/com/greenerlawn/greenerlawn/Schedules.java
+++ app/src/main/java/com/greenerlawn/greenerlawn/Schedules.java
@@ -6,4 +6,62 @@ package com.greenerlawn.greenerlawn;
 
 class Schedules {
 
+    private String schGUID;
+    private int day;
+    private Long startTime;
+    private Long duration;
+    private String zone;
+
+
+    public Schedules() {
+    }
+
+    public Schedules(String schGUID, int day, Long startTime, Long duration, String zone) {
+        this.schGUID = schGUID;
+        this.day = day;
+        this.startTime = startTime;
+        this.duration = duration;
+        this.zone = zone;
+    }
+
+    public String getSchGUID() {
+        return schGUID;
+    }
+
+    public void setSchGUID(String schGUID) {
+        this.schGUID = schGUID;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public Long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Long startTime) {
+        this.startTime = startTime;
+    }
+
+    public Long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Long duration) {
+        this.duration = duration;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public void setZone(String zone) {
+        this.zone = zone;
+    }
+
 }
