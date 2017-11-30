072efae3eb41b824b442d04fe7028f0989597c85
keposet <jsn.hutson@gmail.com>
2017-11-30 13:06:30 -0700

    schedule display activity

---
+++ app/src/main/java/com/greenerlawn/greenerlawn/ScheduleDisplay.java
@@ -0,0 +1,14 @@
+package com.greenerlawn.greenerlawn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class ScheduleDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_display);
+    }
+}
