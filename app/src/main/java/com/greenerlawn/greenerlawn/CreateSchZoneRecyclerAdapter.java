126fbc5e38995203845c5fe22c637e5254b77cf7
keposet <jsn.hutson@gmail.com>
2017-12-04 16:22:01 -0700

    reconfigured buttons for create sched activity created recycler for zone select added proper strings for day select buttons
    created methods for selecting days to pass to data manager

---
+++ app/src/main/java/com/greenerlawn/greenerlawn/CreateSchZoneRecyclerAdapter.java
@@ -0,0 +1,60 @@
+package com.greenerlawn.greenerlawn;
+
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jason on 12/4/2017.
+ */
+
+public class CreateSchZoneRecyclerAdapter extends RecyclerView.Adapter<CreateSchZoneRecyclerAdapter.ViewHolder> {
+    private final LayoutInflater zoneSelectInflater;
+    private final List<CreateSchedule.SchedZoneSelect> zoneSelects;
+    private Activity context;
+
+    public CreateSchZoneRecyclerAdapter( ArrayList<CreateSchedule.SchedZoneSelect> zoneSelects, Activity context) {
+        this.zoneSelectInflater = LayoutInflater.from(context);
+        this.zoneSelects = zoneSelects;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View zoneSelectView = zoneSelectInflater.inflate(R.layout.sch_zone_select_item, parent, false);
+        return new ViewHolder(zoneSelectView);
+    }
+
+    @Override
+    public void onBindViewHolder(CreateSchZoneRecyclerAdapter.ViewHolder holder, int position) {
+        CreateSchedule.SchedZoneSelect selectInstance = zoneSelects.get(position);
+        RadioButton radBTN = holder.select;
+        holder.title.setText(selectInstance.getName());
+        holder.select.setChecked(selectInstance.isSelected());
+    }
+
+    @Override
+    public int getItemCount() {
+        return zoneSelects.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView picture;
+        private TextView title;
+        private RadioButton select;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            picture = (ImageView) itemView.findViewById(R.id.schZoneItem_IMG);
+            title = (TextView) itemView.findViewById(R.id.schZoneItem_TV);
+            select = (RadioButton) itemView.findViewById(R.id.schZoneItem_RB);
+        }
+    }
+}
--- app/src/main/java/com/greenerlawn/greenerlawn/CreateSchedule.java
+++ app/src/main/java/com/greenerlawn/greenerlawn/CreateSchedule.java
@@ -1,13 +1,102 @@
 package com.greenerlawn.greenerlawn;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateSchedule extends AppCompatActivity {
+    private List<Zone> zoneList;
+    private ArrayList<SchedZoneSelect> zoneSelectList = new ArrayList<SchedZoneSelect>();
+    private final boolean NOT_SELECTED = false;
+    private boolean[] dayArray = new boolean[8];
+    private String[] dayTextArr = new String[]{"All", "Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"};
 
+    //todo pull images properly from DB functions
+    //todo pop up for timepicker
+    //todo submit FAB
+    //todo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        zoneList = User.getInstance().zoneListGet();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_schedule);
+        fillZoneSelectList();
+        final RecyclerView selectRecycler = (RecyclerView) findViewById(R.id.zoneSelect_Recycler);
+        final LinearLayoutManager zoneSelLayMan = new LinearLayoutManager(this);
+        selectRecycler.setLayoutManager(zoneSelLayMan);
+
+        CreateSchZoneRecyclerAdapter c = new CreateSchZoneRecyclerAdapter(zoneSelectList,this);
+        selectRecycler.setAdapter(c);
     }
+
+    public void selectDay(View v) {
+        Button dayBTN = (Button) v;
+        dayBTN.getText();
+        int dayValue = 0;
+        for (int i = 0; i < dayTextArr.length; i++) {
+            if (dayBTN.getText().equals(dayTextArr[i])) {
+                dayValue = i;
+            }
+        }
+        if (!dayBTN.isPressed()) {
+            dayBTN.setPressed(true);
+        } else {
+            dayBTN.setPressed(false);
+        }
+        dayArray[dayValue] = dayBTN.isPressed();
+    }
+
+    private void fillZoneSelectList() {
+        for (Zone z : zoneList) {
+            SchedZoneSelect newSZS = new SchedZoneSelect(z.getzImage(), z.getzName(), NOT_SELECTED);
+            zoneSelectList.add(newSZS);
+        }
+    }
+
+    public class SchedZoneSelect {
+        private File zImage;
+        private String name;
+        private boolean selected;
+
+        public SchedZoneSelect(File zImage, String name, boolean selected) {
+            this.zImage = zImage;
+            this.name = name;
+            this.selected = selected;
+        }
+
+        public File getzImage() {
+            return zImage;
+        }
+
+        public void setzImage(File zImage) {
+            this.zImage = zImage;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public boolean isSelected() {
+            return selected;
+        }
+
+        public void setSelected(boolean selected) {
+            this.selected = selected;
+        }
+    }
+
 }
+
+
+
